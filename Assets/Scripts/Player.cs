9e245507bb3c47a45eb56fdb1da951b641e39765
hashbert <obert.chen@gmail.com>
2023-07-07 11:10:57 -0400

    install cinemachine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] private CharacterController _controller;
+
+    [Header("Input")]
+    [SerializeField] private InputActionReference _move;
+    [SerializeField] private InputActionReference _interact;
+
+    [Header("Movement")]
+    [SerializeField] private float _maxWalkSpeed = 5f;
+
+
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
