b9857570c865bc82db2eef2672fab2e48d6f5246
hashbert <obert.chen@gmail.com>
2023-07-11 17:06:59 -0400

    looking smooth, need to fix UI a bit
    also need to fix distance to trigger interact

--- Assets/Scripts/NPCInteractable.cs
+++ Assets/Scripts/NPCInteractable.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using Yarn.Unity;
 
 namespace StarterAssets
 {
     public class NPCInteractable : MonoBehaviour, IInteractable
     {
+        public event Action<NPCInteractable> OnInteractPossible;
         [SerializeField] private string _interactText;
+        [SerializeField] private string _startNode;
         public bool InteractPossible { get; private set; }
         [SerializeField] private Image _exclamationMark;
-        //[SerializeField] private Rigidbody _rb;
+        [SerializeField] private DialogueRunner _dialogueRunner;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -20,6 +23,7 @@ namespace StarterAssets
             {
                 InteractPossible = true;
                 print("interact possible" + InteractPossible);
+                OnInteractPossible?.Invoke(this);
                 _exclamationMark.enabled = true;
             }
         }
@@ -37,6 +41,7 @@ namespace StarterAssets
         public void Interact(Transform interactorTransform)
         {
             print("hey buddy, how can I help" + gameObject.name);
+            _dialogueRunner.StartDialogue(_startNode);
         }
 
         public string GetInteractText()
