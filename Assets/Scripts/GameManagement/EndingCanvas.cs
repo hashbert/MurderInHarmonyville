c6ea1bfea9c4e83f2bbfe803b9e7dc4a4010d586
hashbert <obert.chen@gmail.com>
2023-07-28 15:24:40 -0400

    should be fully controller compatible

--- Assets/Scripts/GameManagement/EndingCanvas.cs
+++ Assets/Scripts/GameManagement/EndingCanvas.cs
@@ -3,22 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using Yarn.Unity;
 
 public class EndingCanvas : MonoBehaviour
 {
     [SerializeField] private CanvasGroup _endgameCanvasGroup;
     [SerializeField] private float _fadeInTime = 2f;
-    [SerializeField] private GameObject _interactImageAndText;
+    [SerializeField] private GameObject _UICanvas;
     [SerializeField] private PlayerInput _playerInput;
     [SerializeField] private AudioSource _mainTheme;
     [SerializeField] private AudioSource _introAndWinTheme;
+    [SerializeField] private Button _playButton; 
 
     [YarnCommand("PlayEndCanvas")]
     public void PlayEndCanvas()
     {
         _endgameCanvasGroup.interactable = true;
         _endgameCanvasGroup.blocksRaycasts = true;
+        _playButton.Select();
         LeanTween.value(_endgameCanvasGroup.gameObject, SetAlphaCallback, 0f, 1f, _fadeInTime).setEase(LeanTweenType.easeOutQuint);
         StartCoroutine(TurnOffInteractAndText());
         _mainTheme.Stop();
@@ -28,7 +31,7 @@ public class EndingCanvas : MonoBehaviour
     private IEnumerator TurnOffInteractAndText()
     {
         yield return new WaitForEndOfFrame();
-        _interactImageAndText.SetActive(false);
+        _UICanvas.SetActive(false);
         _playerInput.SwitchCurrentActionMap("UI");
     }
 
